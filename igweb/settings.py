6b0f80d0d59a9b3fe1272f7ea0f217efe12a8738
Ohsaem <ss080026@naver.com>
2022-08-20 01:20:31 +0900

    배포를 위한 commit 추가

--- igweb/settings.py
+++ igweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
 #from .my_settings import mySECRET_KEY, myDATABASES
 import os
 
