4567eaeee80813cee8eb86b877263fd70eab1be6
Ohsaem <ss080026@naver.com>
2022-08-20 01:36:03 +0900

    배포를 위한 commit 추가 3

--- igweb/settings.py
+++ igweb/settings.py
@@ -188,8 +188,8 @@ SUMMERNOTE_CONFIG = {
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, 'static'),
+#]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
