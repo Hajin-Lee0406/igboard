65df39ada9e12e1929f6befa0ca9f2405961ed0c
Ohsaem <ss080026@naver.com>
2022-08-20 00:55:59 +0900

    배포를 위한 commit 7

--- igweb/settings.py
+++ igweb/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 #from .my_settings import mySECRET_KEY, myDATABASES
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,7 +154,6 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 INSTALLED_APPS += ['django_summernote']
 
-import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
