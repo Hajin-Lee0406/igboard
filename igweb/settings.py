b7cf60be1e6f56865a9b3d3c645a82904c7c136d
Ohsaem <ss080026@naver.com>
2022-08-20 00:39:21 +0900

    배포를 위한 commit 3

--- igweb/settings.py
+++ igweb/settings.py
@@ -103,7 +103,8 @@ DATABASES = {
         'OPTIONS':{
             'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
         }
-
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
