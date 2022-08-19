a0ca4e941ebe8de838a0cbf300a3cc6270a477e8
Ohsaem <ss080026@naver.com>
2022-08-20 02:00:35 +0900

    배포를 위한 commit 추가 4

--- igweb/settings.py
+++ igweb/settings.py
@@ -95,19 +95,19 @@ WSGI_APPLICATION = 'igweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-		'NAME': get_env_variable('DATABASE'),
-        'USER': get_env_variable('DB_USER'),
-        'PASSWORD': get_env_variable('DB_PASSWORD'),
-        'HOST': get_env_variable('DB_HOST'),
-        'PORT': get_env_variable('DB_PORT'),
-        'OPTIONS':{
-            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
-}
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.mysql',
+		#'NAME': get_env_variable('DATABASE'),
+        #'USER': get_env_variable('DB_USER'),
+        #'PASSWORD': get_env_variable('DB_PASSWORD'),
+        #'HOST': get_env_variable('DB_HOST'),
+        #'PORT': get_env_variable('DB_PORT'),
+        #'OPTIONS':{
+            #'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        #}
+    #}
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
